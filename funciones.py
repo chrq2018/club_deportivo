53a0c1a4b1e1d7a67e069e2a856b77443f149586
chrq2018 <quirogachristian@gmail.com>
2024-06-17 20:21:20 -0300

    v2_1706

--- funciones.py
+++ funciones.py
@@ -1,7 +1,12 @@
 import pyodbc
-from Conexion import *
-from entidades import *
 import getpass
+
+def conexion_sql_server():
+    try:
+        conexion = 'DRIVER={SQL SERVER}; SERVER=DESKTOP-7IL9JF9\\SQLEXPRESS;DATABASE=CLUB_DEPORTIVO;Trust_Connection={yes}; UID=sa; PWD=123'                    
+    except ConnectionError as e:
+                print("Ocurrio un error al conectar SQL Server: ", e)
+    return conexion
 
 def menu_gestion_empleado():
     print()
@@ -15,7 +20,6 @@ def menu_gestion_empleado():
         print('5) Volver al menu login!')
         opc = int(input('Elija una opción correcta: '))
     return opc
-    print()
 
 def menu_gestion_gerente():
     print()
@@ -47,10 +51,10 @@ def validar_inicio_sesion():
             usuario = input("Ingrese usuario: ")
             #password = input("Ingrese password: ")
             cantLetras = 2
-            password = getpass.getpass("Enter your password: ")
-            while len(password) < cantLetras: 
-                password = getpass.getpass("Enter your password (cantidad de caracteres debe ser mayor a 2): ")
-            resultado = Usuario.iniciar_sesion(usuario, password)
+            password = getpass.getpass("Ingrese password: ")
+            """while len(password) < cantLetras: 
+                password = getpass.getpass("Ingrese password (cantidad de caracteres debe ser mayor a 2): ")"""
+            resultado = iniciar_sesion(usuario, password)
             if resultado:
                 print("Ingreso correcto")
                 cont = 4
@@ -60,3 +64,91 @@ def validar_inicio_sesion():
                     print("Usuario bloqueado, comuniquese con el administrador del sistema!!")    
                 cont += 1 
         return resultado
+
+def iniciar_sesion(usuario, clave):
+        conn = pyodbc.connect(conexion_sql_server())
+        cursor = conn.cursor()
+        sql ="select * from Usuarios where Usuario = ? and Pass = ?;"
+        cursor.execute(sql, (usuario, clave))
+        resultado = cursor.fetchone()
+        return resultado
+
+def alta(nombre, apellido, telefono, deporte, tipo_de_cliente):
+    try:
+        conn = pyodbc.connect(conexion_sql_server())
+        cursor = conn.cursor()
+        sql = "insert into Clientes (Nombre, Apellido, Telefono, Deporte, Tipo_de_cliente) values (?, ?, ?, ?, ?);"
+        valores = (nombre, apellido, telefono, deporte, tipo_de_cliente)
+        cursor.execute(sql,valores)
+        conn.commit()
+        print(cursor.rowcount,"Registro ingresado")
+        conn.close()
+    except Exception as e:
+        print("Error!, no se pudo dar de alta{}".format(e))
+
+def lista():
+    try:
+        conn = pyodbc.connect(conexion_sql_server())
+        cursor = conn.cursor()
+        sql = "select * from Clientes;"
+        cursor.execute(sql)
+        resultado = cursor.fetchone()
+        while resultado:
+            print(resultado)
+            resultado = cursor.fetchone()
+        conn.close()
+    except Exception as e:
+        print("Error!, no se pudo dar de alta{}".format(e))
+
+def modificar():
+    while True:
+        actualizar = input("\nQuiere actualizar un dato s/n: ")
+        if actualizar.lower() == "s":
+            try:
+                conn = pyodbc.connect(conexion_sql_server())
+                cursor = conn.cursor()
+                idS = input("Ingrese el ID del socio que desea actualizar: ")
+                sql ="select id_cliente from Clientes where Id_cliente = ?;"
+                cursor.execute(sql, idS)
+                resultado = cursor.fetchone()
+                if resultado:
+                    deporte = input("Ingrese el nuevo deporte: ")
+                    sql = "update Clientes set deporte = ? where Id_cliente = ?;"
+                    cursor.execute(sql, (deporte, idS))
+                    conn.commit()
+                else:
+                    print("El ID ingresado es incorrecto!!")
+                conn.close()
+            except Exception as e:
+                print("Error!, no se pudo realizar la mofificacion{}".format(e))
+        else:
+            print("Salio de la opcion modificar")
+            break
+
+def baja():
+    while True:
+        eliminar = input("\nQuiere eliminar un socio s/n: ")
+        if eliminar.lower() == "s":
+            try:
+                conn = pyodbc.connect(conexion_sql_server())
+                cursor = conn.cursor()
+                idS = input("Ingrese el ID del cliente que desea eliminar: ")
+                consultaV ="select Id_cliente from Clientes where id_cliente = ?;"
+                cursor.execute(consultaV, idS)
+                resultado = cursor.fetchone()
+                if resultado:
+                    consultaD = "delete from Clientes where id_cliente = ?;"
+                    cursor.execute(consultaD, (idS))
+                    conn.commit()
+                else:
+                    print("El ID ingresado es incorrecto!!")
+                conn.close()
+            except Exception as e:
+                print("Error!, no se pudo realizar la baja{}".format(e))
+        else:
+            print("Salio de la opcion Baja")
+            break    
+
+def mostrar_informe():
+    pass 
+                                 
--- main.py
+++ main.py
@@ -9,9 +9,9 @@ considerar según el grupo de trabajo)
 4) Rendición de cuentas mensuales
 """
 
-from Conexion import *
+
 from funciones import *
-from entidades import *
+
 import getpass
 #Programa principal
 
@@ -29,16 +29,20 @@ while op3 != 2:
                     if op2 == 1:
                         print("***Dar de alta un Socio***")
                         nombre = input("Ingrese el nombre: ")
-                        Usuario.alta(nombre)
+                        apellido = input("Ingrese el apeellido: ")
+                        telefono = input("Ingrese el telefono: ")
+                        deporte = int(input("Ingrese el deporte: "))
+                        tipo_de_cliente = int(input("Ingrese el tipo de cliente: "))
+                        alta(nombre, apellido, telefono, deporte, tipo_de_cliente)
                     elif op2 == 2:
                         print("***Dar de baja un socio***")
-                        Usuario.baja()
+                        baja()
                     elif op2 == 3:
                         print("Modificar los datos de un socio")
-                        Usuario.modificar()
+                        modificar()
                     elif op2 == 4:
                         print("***Lista de Socios***\n")
-                        Usuario.lista()  
+                        lista()  
                     else:
                         print("Volver al menu login!")
                         
@@ -49,23 +53,24 @@ while op3 != 2:
                     if op2 == 1:
                         print("***Dar de alta un Socio***")
                         nombre = input("Ingrese el nombre: ")
-                        Usuario.alta(nombre)
+                        alta(nombre)
                     elif op2 == 2:
                         print("***Dar de baja un socio***")
-                        Usuario.baja()
+                        baja()
                     elif op2 == 3:
                         print("Modificar los datos de un socio")
-                        Usuario.modificar()
+                        modificar()
                     elif op2 == 4:
                         print("***Lista de Socios***\n")
-                        Usuario.lista()  
+                        lista()  
                     elif op2 == 5:
                         print("Mostrar informe mensual")
-                        Usuario.mostrar_informe() 
+                        mostrar_informe() 
                     else:
                         print("Volver al menu login!")
+                
         else:
-            op3 = 2
+            op3 = menu_login()
 else:
     print("FIN!")
         
